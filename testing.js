e47e329809c34e4183476da81a08efeddc2fc6f6
mahwishriazjamil <mahwishcee@gmail.com>
2025-02-03 14:23:41 +0000

    first commit

---
+++ testing.js
@@ -0,0 +1,14 @@
+//Generate a function to add two numbers
+function add(a, b) {
+  return a + b;
+}
+
+add(a, b);
+
+//Generate an array of all the colors from the rainbow
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// print out the colors in rainbow array
+rainbow.forEach((color) => {
+  console.log(color);
+});
